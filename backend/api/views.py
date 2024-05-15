c61a39be84aab09bb0bad0c96d809891640de521
Vip Monty <10835390@uvu.edu>
2024-05-15 15:43:43 -0600

    1:20:33 @ https://youtu.be/c708Nf0cHrs

--- backend/api/views.py
+++ backend/api/views.py
@@ -8,8 +8,13 @@ from rest_framework.decorators import api_view
 from products.models import Product
 from products.serializers import ProductSerializer
 # Create your views here.
-@api_view(["GET"])
+@api_view(["POST"])
 def api_home(request, *args, **kwargs):
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid():
+        instance = serializer.save()
+        print(instance)
+        return Response(serializer.data)
     """This is now a DJANGO REST FRAMEWORK API VIEW WHEN WE MAKE THE REST_FRAMEWORK IMPORTS ABOVE"""
     #request -> HttpRequest -> Django
     #request.body is from the Django package
@@ -17,12 +22,13 @@ def api_home(request, *args, **kwargs):
     # print(f"POST param test: {request.POST}" )
     # body = request.body # JSON data in byte string which look like this on the cmd: b'{"query": "Hello world"}'
     #=====================converting byte str to JSON =========================================
-    if request .method != "Post":
-        Response({"detail":"You are not allow to post from this server"}, status=405)
-    instance = Product.objects.all().order_by("?").first()
-    data = {} #<==converting the byte string to JSON AND STORING THE CONTENT INTO PYTHON DICT
-    if instance:
-        data = ProductSerializer(instance).data
+    # data = request.data
+# #========================================================================
+#     instance = Product.objects.all().order_by("?").first()
+#     data = {} #<==converting the byte string to JSON AND STORING THE CONTENT INTO PYTHON DICT
+#     if instance:
+#         data = ProductSerializer(instance).data
+#========================================================================
         # data = model_to_dict(instance, fields=['id','title', 'price', 'sale_price']) #<<====You can now specify which fiend you want to extract here
         # data['id'] = model_data.id
         # data['title'] = model_data.title
@@ -44,4 +50,4 @@ def api_home(request, *args, **kwargs):
     #===================================================================
     # print(body)#<==== body of request in byte str
     # return JsonResponse({"message": "Hello old friend"}) <===If uncommented it will just return hard code message
-    return Response(data) #<========this will return the Jason response in python dict when requested by basic.py
+    # return Response(data) #<========this will return the Jason response in python dict when requested by basic.py
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,7 +4,7 @@ import requests
 # endpoint = "https://httpbin.org/status/200/"
 endpoint = "http://localhost:8000/api/" #this endpoint is a working real endpoint for our project purposes
 
-get_response = requests.get(endpoint, params={"abc":123}, json={"query":"Hello world"}) # GET/HTTP request for WEB API aka Application programming interface
+get_response = requests.post(endpoint, json={"title": "Viphakone Monty","content":"dog"}) # GET/HTTP request for WEB API aka Application programming interface
 
 # print(get_response.text) # print raw text response in HTML format dependinhg on Endpoint
 print(get_response.json()) # this will give us endpoint response in json format, which is like python dictionary
