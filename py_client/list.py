413ba80fe8c2211b22eeb43a65cd6c07a0e146da
Viphakone Monty <10835390@my.uvu.edu>
2024-05-22 22:14:34 -0600

    updated list.py with pretty_response to display json

--- py_client/list.py
+++ py_client/list.py
@@ -1,6 +1,13 @@
 import requests
+import json
 
 endpoint = "http://localhost:8000/api/products/"
 
 get_response = requests.get(endpoint)
-print(get_response.json())
+
+if get_response.status_code == 200:
+    response_data = get_response.json()
+    pretty_response = json.dumps(response_data, indent=4)
+    print(pretty_response)
+else:
+    print(f"Failed to retrieve data: {get_response.status_code}")
