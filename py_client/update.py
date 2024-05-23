0f89870c6c6ae7ab86970d0cab574827d8b87c1a
Viphakone Monty <10835390@my.uvu.edu>
2024-05-22 21:40:20 -0600

    made addtional updates to update.py

--- py_client/update.py
+++ py_client/update.py
@@ -4,9 +4,14 @@ pk = int(input('Please provide Id of row you want to update.'))
 
 endpoint = f"http://localhost:8000/api/products/{pk}/update/"
 title_input = input('Please provide product title')
+field_prompts = f"title, content, price, sale_price, discount, test_field, vip"
+print(field_prompts)
+field_updater = input('choose from provided fields to update.')
+update_prompt = input(f"You chose {field_updater}, Now provide Value:")
+
 data = {
     "title": f"{title_input}",
-    # "price": 129.00
+    f"{field_updater}": f"{update_prompt}"
 }
 get_response = requests.put(endpoint, json=data)
 print(get_response.json())
